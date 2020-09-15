dec554b76b0cdba44322e7a10d226888092c857d
lesiak <lesiak@outlook.com>
2020-09-15 10:19:26 +0200

    [DRIVE] Added project Google.Apis.Drive.Sample

---
+++ dotnet/Google.Apis.Drive.Sample/Google.Apis.Drive.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Google.Apis.Drive.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
