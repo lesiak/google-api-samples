06c0d416fcec7b8c6b134b0218042f09f5f30bf1
lesiak <lesiak@outlook.com>
2020-09-15 10:43:42 +0200

    [DRIVE] Enumerate files
    https://developers.google.com/drive/api/v3/quickstart/dotnet

--- dotnet/Google.Apis.Drive.Sample/Google.Apis.Drive.Sample/Program.cs
+++ dotnet/Google.Apis.Drive.Sample/Google.Apis.Drive.Sample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
+using Google.Apis.Services;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Google.Apis.Json;
 
@@ -45,7 +47,32 @@ namespace Google.Apis.Drive.Sample
         {
             var credential = GetServiceAccountCredential("credentials.json", null);
 
-            Console.WriteLine(credential);
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+            Console.WriteLine("Files:");
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
 
             Console.Read();
         }
